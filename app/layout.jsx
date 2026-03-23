7bfee31033c47340a31808ceff8d7734424f7ba8
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-03-23 12:19:03 +0530

    Added Clerk

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,16 +2,18 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-    title: "GoCart. - Shop smarter",
-    description: "GoCart. - Shop smarter",
+  title: "GoCart. - Shop smarter",
+  description: "GoCart. - Shop smarter",
 };
 
 export default function RootLayout({ children }) {
-    return (
+  return (
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${outfit.className} antialiased`}
@@ -23,5 +25,6 @@ export default function RootLayout({ children }) {
           </StoreProvider>
         </body>
       </html>
-    );
+    </ClerkProvider>
+  );
 }
